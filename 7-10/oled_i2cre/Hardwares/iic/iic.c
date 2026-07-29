05e9a454e121eca1578e9c2512cfa6346e41260f
TMLG <1694462592@qq.com>
2026-07-29 09:46:54 +0800

    IIC软件模拟项目新建

---
+++ 7-10/oled_i2cre/Hardwares/iic/iic.c
@@ -0,0 +1,38 @@
+#include "iic.h"
+void IIC_INIT()
+{
+
+
+
+
+}
+
+
+
+
+
+void IIC_START()
+{
+
+
+
+}
+
+
+void IIC_COMMUNICATE()
+{
+
+
+
+
+}
+
+
+void IIC_END()
+{
+
+
+
+
+
+}
---
+++ 7-10/oled_i2cre/Hardwares/iic/iic.h
@@ -0,0 +1,18 @@
+#ifndef __IIC_H__
+#define __IIC_H__
+#include "main.h"
+
+#define IIC_SCL_PORT GPIOB
+#define IIC_SCL_PIN  GPIO_PIN_6
+
+#define IIC_SDA_PORT GPIOB
+#define IIC_SDA_PIN  GPIO_PIN_7
+
+#define SCL_HIGH HAL_GPIO_WritePin(IIC_SCL_PORT,IIC_SCL_PIN,1)
+#define SCL_LOW HAL_GPIO_WritePin(IIC_SCL_PORT,IIC_SCL_PIN,0)
+
+#define SDA_HIGH HAL_GPIO_WritePin(IIC_SDA_PORT,IIC_SDA_PIN,1)
+#define SDA_LOW HAL_GPIO_WritePin(IIC_SDA_PORT,IIC_SDA_PIN,0)
+
+
+#endif
